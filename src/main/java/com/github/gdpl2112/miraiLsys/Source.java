188ee31aace448305f497efdb0a4f38a22a9c25d
HRS-Computer <3474006766@qq.com>
2022-06-08 19:01:02 +0800

    update

--- src/main/java/com/github/gdpl2112/miraiLsys/Source.java
+++ src/main/java/com/github/gdpl2112/miraiLsys/Source.java
@@ -7,7 +7,7 @@ import cn.kloping.lsys.entitys.User;
 import io.github.kloping.arr.Class2OMap;
 import kotlin.jvm.functions.Function2;
 import net.mamoe.mirai.contact.MemberPermission;
-import net.mamoe.mirai.event.events.GroupMessageEvent;
+import net.mamoe.mirai.event.events.GroupAwareMessageEvent;
 import net.mamoe.mirai.event.events.MemberJoinRequestEvent;
 import net.mamoe.mirai.message.data.At;
 
@@ -40,8 +40,8 @@ public class Source {
     public static final Function2<User, Request, Result> SET_THE_TITLE = new Function2<User, Request, Result>() {
         @Override
         public Result invoke(User user, Request request) {
-            if (request.getEvent() instanceof GroupMessageEvent) {
-                GroupMessageEvent gme = (GroupMessageEvent) request.getEvent();
+            if (request.getEvent() instanceof GroupAwareMessageEvent) {
+                GroupAwareMessageEvent gme = (GroupAwareMessageEvent) request.getEvent();
                 int l = gme.getGroup().get(gme.getBot().getId()).getPermission().getLevel();
                 if (l == MemberPermission.OWNER.getLevel()) {
                     String name = request.getStr().substring(request.getOStr().indexOf(".")).trim();
@@ -62,8 +62,8 @@ public class Source {
     public static final Function2<User, Request, Result> SET_THE_ADMIN = new Function2<User, Request, Result>() {
         @Override
         public Result invoke(User user, Request request) {
-            if (request.getEvent() instanceof GroupMessageEvent) {
-                GroupMessageEvent gme = (GroupMessageEvent) request.getEvent();
+            if (request.getEvent() instanceof GroupAwareMessageEvent) {
+                GroupAwareMessageEvent gme = (GroupAwareMessageEvent) request.getEvent();
                 int l = gme.getGroup().get(gme.getBot().getId()).getPermission().getLevel();
                 if (l == MemberPermission.OWNER.getLevel()) {
                     if (user.getQq().longValue() == Resource.conf.getQq().longValue()) {
@@ -88,8 +88,8 @@ public class Source {
     public static final Function2<User, Request, Result> UN_THE_ADMIN = new Function2<User, Request, Result>() {
         @Override
         public Result invoke(User user, Request request) {
-            if (request.getEvent() instanceof GroupMessageEvent) {
-                GroupMessageEvent gme = (GroupMessageEvent) request.getEvent();
+            if (request.getEvent() instanceof GroupAwareMessageEvent) {
+                GroupAwareMessageEvent gme = (GroupAwareMessageEvent) request.getEvent();
                 int l = gme.getGroup().get(gme.getBot().getId()).getPermission().getLevel();
                 if (l == MemberPermission.OWNER.getLevel()) {
                     if (user.getQq().longValue() == Resource.conf.getQq().longValue()) {
@@ -113,14 +113,14 @@ public class Source {
     public static final Function2<User, Request, Result> KICK_ONE = new Function2<User, Request, Result>() {
         @Override
         public Result invoke(User user, Request request) {
-            if (request.getEvent() instanceof GroupMessageEvent) {
-                GroupMessageEvent gme = (GroupMessageEvent) request.getEvent();
+            if (request.getEvent() instanceof GroupAwareMessageEvent) {
+                GroupAwareMessageEvent gme = (GroupAwareMessageEvent) request.getEvent();
                 int l = gme.getGroup().get(gme.getBot().getId()).getPermission().getLevel();
                 At at = Class2OMap.create(request.getEvent().getMessage()).get(At.class);
                 if (at == null) {
                     return state2;
                 }
-                if (gme.getPermission().getLevel() > gme.getGroup().get(at.getTarget()).getPermission().getLevel()) {
+                if (gme.getGroup().get(gme.getSender().getId()).getPermission().getLevel() > gme.getGroup().get(at.getTarget()).getPermission().getLevel()) {
                     gme.getGroup().get(at.getTarget()).kick("->");
                     return state0;
                 } else {
@@ -138,10 +138,10 @@ public class Source {
     public static final Function2<User, Request, Result> AGREE_REQUEST = new Function2<User, Request, Result>() {
         @Override
         public Result invoke(User user, Request request) {
-            if (request.getEvent() instanceof GroupMessageEvent) {
-                GroupMessageEvent event = (GroupMessageEvent) request.getEvent();
+            if (request.getEvent() instanceof GroupAwareMessageEvent) {
+                GroupAwareMessageEvent event = (GroupAwareMessageEvent) request.getEvent();
                 if (user.getQq().longValue() == Resource.conf.getQq().longValue()
-                        || event.getSender().getPermission().getLevel() > 0) {
+                        || event.getGroup().get(event.getSender().getId()).getPermission().getLevel() > 0) {
                     if (MEMBER_JOIN_REQUEST_EVENTS.isEmpty()) {
                         return state2;
                     } else {
@@ -159,10 +159,10 @@ public class Source {
     public static final Function2<User, Request, Result> REJECT_REQUEST = new Function2<User, Request, Result>() {
         @Override
         public Result invoke(User user, Request request) {
-            if (request.getEvent() instanceof GroupMessageEvent) {
-                GroupMessageEvent event = (GroupMessageEvent) request.getEvent();
+            if (request.getEvent() instanceof GroupAwareMessageEvent) {
+                GroupAwareMessageEvent event = (GroupAwareMessageEvent) request.getEvent();
                 if (user.getQq().longValue() == Resource.conf.getQq().longValue()
-                        || event.getSender().getPermission().getLevel() > 0) {
+                        || event.getGroup().get(event.getSender().getId()).getPermission().getLevel() > 0) {
                     if (MEMBER_JOIN_REQUEST_EVENTS.isEmpty()) {
                         return state2;
                     } else {
