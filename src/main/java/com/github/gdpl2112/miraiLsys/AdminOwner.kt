c2fe910e8ec19ca35df0ac839eccdae69fbf0466
HRS-Computer <3474006766@qq.com>
2022-09-03 12:49:33 +0800

    update

--- src/main/java/com/github/gdpl2112/miraiLsys/AdminOwner.kt
+++ src/main/java/com/github/gdpl2112/miraiLsys/AdminOwner.kt
@@ -14,7 +14,7 @@ object AdminOwner :
         JvmPluginDescriptionBuilder("com.github.gdpl2112.lsys.AdminOwner", 1.5.toString())
             .author("github-kloping")
             .info("lsys群管插件")
-            .dependsOn("cn.kloping.Lsys", "1.6", true)
+            .dependsOn("cn.kloping.Lsys", "1.7", true)
             .build()
     ) {
 
